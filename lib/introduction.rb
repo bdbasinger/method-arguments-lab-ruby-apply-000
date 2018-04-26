d1d9ed2506cc0ef13ba9e2829d78acfe48eec901
Brennan B <bdbasinger777@gmail.com>
2018-04-26 06:32:32 +0000

    Done

---
+++ lib/introduction.rb
@@ -0,0 +1,21 @@
+#1. Define a method that takes in an argument and uses that argument in the #method body. 
+#2. Define a method that takes in two arguments and uses both arguments in #the method body. 
+
+## Instructions
+
+#You'll be coding your methods in `lib/introduction.rb`. 
+
+### The `#introduction` Method
+
+#Run the test suite to get started. To do that, run `learn` or `learn test` #in your terminal. Let's take a look at the first error:
+
+
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
